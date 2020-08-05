3a25cb70bd599e0b0782c440f606998f41a3867b
Michael Thaler <michael.thaler@dlr.de>
2020-08-05 08:48:46 +0200

    Updated logicalexpression_test.go.

--- expr/logicalexpression_test.go
+++ expr/logicalexpression_test.go
@@ -2,6 +2,7 @@ package expr
 
 import (
 	"aparser"
+	"aparser/ast"
 	"testing"
 )
 
@@ -10,7 +11,11 @@ func TestLogicalExpression(t *testing.T) {
 	b := aparser.CreateBuffer("false || true")
 	checkRead(t, b, e)
 	checkPosition(t, b, 13)
+	a := ast.CreateAST(b)
+	checkEvaluate(t, a, true)
 	b = aparser.CreateBuffer("false || (true && false)")
 	checkRead(t, b, e)
 	checkPosition(t, b, 24)
+	//a = ast.CreateAST(b)
+	//checkEvaluate(t, a, true)
 }
