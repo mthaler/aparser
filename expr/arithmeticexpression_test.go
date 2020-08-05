0b11c53c7bb4fba27c6aad010fe363c2a50a5e12
Michael Thaler <michael.thaler@dlr.de>
2020-08-05 14:03:00 +0200

    Added TestArithmeticExpressionSimple.

--- expr/arithmeticexpression_test.go
+++ expr/arithmeticexpression_test.go
@@ -6,13 +6,37 @@ import (
 	"testing"
 )
 
-func Test_arithmeticExpression_Read_doubleLiteral(t *testing.T) {
+func TestArithmeticExpressionLiteral(t *testing.T) {
 	e := arithmeticExpression()
 	b := aparser.CreateBuffer("3.14")
 	checkRead(t, b, e)
 	checkPosition(t, b, 4)
 	a := ast.CreateAST(b)
 	checkEvaluate(t, a, 3.14)
+}
+
+func TestArithmeticExpressionSimple(t *testing.T) {
+	e := arithmeticExpression()
+	b := aparser.CreateBuffer("3 + 4")
+	checkRead(t, b, e)
+	checkPosition(t, b, 5)
+	a := ast.CreateAST(b)
+	checkEvaluate(t, a, 7.0)
+	b = aparser.CreateBuffer("3 - 4")
+	checkRead(t, b, e)
+	checkPosition(t, b, 5)
+	a = ast.CreateAST(b)
+	checkEvaluate(t, a, -1.0)
+	b = aparser.CreateBuffer("3 * 4")
+	checkRead(t, b, e)
+	checkPosition(t, b, 5)
+	a = ast.CreateAST(b)
+	checkEvaluate(t, a, 12.0)
+	b = aparser.CreateBuffer("3 / 4")
+	checkRead(t, b, e)
+	checkPosition(t, b, 5)
+	a = ast.CreateAST(b)
+	checkEvaluate(t, a, 0.75)
 }
 
 func Test_arithmeticExpression(t *testing.T) {
