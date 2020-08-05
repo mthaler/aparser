82bb63bf28b9b1d8c70eb3978c99917c0b46cf54
Michael Thaler <michael.thaler@dlr.de>
2020-08-05 18:17:09 +0200

    Updated arithmeticexpression.go.

--- expr/arithmeticexpression.go
+++ expr/arithmeticexpression.go
@@ -22,13 +22,9 @@ func arithmeticExpression() recursiveExpression {
 	group := and(ws, charLiteral('('), ws, &expression, charLiteral(')'), ws)
 	operand2 := or(number, function, group)
 	operand.value = or(and(neg, &operand), operand2)
-	powerOp := and(ws, exp, ws, &operand, ws)
-	power := and(ws, &operand, ws, zeroOrMore(powerOp))
-	mulDivOp := and(ws, or(mul, div), ws, power, ws)
-	term := and(ws, power, ws, zeroOrMore(mulDivOp))
-	addSubOp := and(ws, or(add, sub), ws, term, ws)
-	term2 := and(ws, term, ws, zeroOrMore(addSubOp))
-	expression.value = term2
+	power := and(ws, &operand, ws, zeroOrMore(and(ws, exp, ws, &operand, ws)))
+	term := and(ws, power, ws, zeroOrMore(and(ws, or(mul, div), ws, power, ws)))
+	expression.value = and(ws, term, ws, zeroOrMore(and(ws, or(add, sub), ws, term, ws)))
 
 	number.SetCreateNode(ast.CreateDoubleOperand)
 	add.SetCreateNode(ast.CreateBinaryOperation)
