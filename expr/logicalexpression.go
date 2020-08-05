bab0b033c5891242a7e7fda75da33fc0248611f3
Michael Thaler <michael.thaler@dlr.de>
2020-08-05 12:33:27 +0200

    Removed commented out code.

--- expr/logicalexpression.go
+++ expr/logicalexpression.go
@@ -31,30 +31,3 @@ func logicalExpression() recursiveExpression {
 
 	return term
 }
-
-//func logicalExpression() recursiveExpression {
-//	term := recursive()
-//
-//	t := stringLiteral("true")
-//	f := stringLiteral("false")
-//	ws := optionalWhiteSpaces()
-//	boolean := and(ws, and(or(t, f), ws))
-//
-//	a := and(ws, stringLiteral("&&"), ws)
-//	o := and(ws, stringLiteral("||"), ws)
-//	xor := and(ws, charLiteral('^'), ws)
-//
-//	op := and(or(a, o, xor))
-//
-//	term.value = or(op, boolean)
-//
-//	boolean.SetCreateNode(ast.CreateBooleanOperand)
-//	a.SetCreateNode(ast.CreateBinaryOperation)
-//	o.SetCreateNode(ast.CreateBinaryOperation)
-//	xor.SetCreateNode(ast.CreateBinaryOperation)
-//	term.SetCreateNode(ast.CreateBinaryLeftAssoc)
-//
-//	boolean.SetId("bool")
-//
-//	return term
-//}
