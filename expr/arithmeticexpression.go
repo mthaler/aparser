39aae4bab70402255feacf78c1919f740764964d
Michael Thaler <michael.thaler@dlr.de>
2020-08-08 09:07:30 +0200

    Updated arithmeticexpression.go.

--- expr/arithmeticexpression.go
+++ expr/arithmeticexpression.go
@@ -4,54 +4,100 @@ import "aparser/ast"
 
 func arithmeticExpression() recursiveExpression {
 
-	expression := recursive()
-	operand := recursive()
-
 	ws := optionalWhiteSpaces()
-	number := and(ws, doubleLiteral(), ws)
-	funcName := identifier()
 
-	add := and(ws, charLiteral('+'), ws)
-	sub := and(ws, charLiteral('-'), ws)
-	mul := and(ws, charLiteral('*'), ws)
-	div := and(ws, charLiteral('/'), ws)
-	exp := and(ws, charLiteral('^'), ws)
-	neg := and(ws, charLiteral('-'), ws)
+	t := stringLiteral("true")
+	f := stringLiteral("false")
 
-	function := and(funcName, charLiteral('('), &expression, charLiteral(')'))
-	group := and(ws, charLiteral('('), ws, &expression, charLiteral(')'), ws)
-	operand2 := or(number, function, group)
-	operand.value = or(and(neg, &operand), operand2)
-	power := and(ws, &operand, ws, zeroOrMore(and(ws, exp, ws, &operand, ws)))
-	term := and(ws, power, ws, zeroOrMore(and(ws, or(mul, div), ws, power, ws)))
-	expression.value = and(ws, term, ws, zeroOrMore(and(ws, or(add, sub), ws, term, ws)))
+	num := and(ws, doubleLiteral(), ws)
+	boolean := and(ws, or(t, f), ws)
+	_string := quotedString("\"", "")
 
-	number.SetCreateNode(ast.CreateDoubleOperand)
+	funcname := and(ws, identifier(), ws)
+	_if := stringLiteral("if")
+
+	neg := charLiteral('-')
+
+	add := charLiteral('+')
+	sub := charLiteral('-')
+	mul := charLiteral('*')
+	div := charLiteral('/')
+	exp := charLiteral('^')
+	mod := charLiteral('%')
+
+	a := stringLiteral("&&")
+	o := stringLiteral("||")
+	not := charLiteral('!')
+	xor := charLiteral('^')
+
+	lt := charLiteral('<')
+	le := stringLiteral("<=")
+	ge := stringLiteral(">=")
+	gt := charLiteral('>')
+
+	eq := stringLiteral("==")
+	ne := stringLiteral("!=")
+
+	o7 := and(ws, or(neg, not), ws)
+	o6 := and(ws, exp, ws)
+	o5 := and(ws, or(mul, div, mod), ws)
+	o4 := and(ws, or(add, sub), ws)
+	o3 := and(ws, or(le, ge, lt, gt), ws)
+	o2 := and(ws, or(eq, ne), ws)
+	o1 := and(ws, or(a, o, xor), ws)
+
+	cond := recursive()
+	function := and(funcname, charLiteral('('), ws, &cond, ws, zeroOrMore(and(ws, charLiteral(','), &cond)), charLiteral(')'))
+	p100 := or(boolean, num, _string, function, and(charLiteral('('), &cond, charLiteral(')')))
+	p7 := recursive()
+	p7.value = or(and(o7, &p7, ws), p100)
+	p6 := and(ws, p7, ws, zeroOrMore(and(o6, p7, ws)))
+	p5 := and(ws, p6, ws, zeroOrMore(and(o5, p6, ws)))
+	p4 := and(ws, p5, ws, zeroOrMore(and(o4, p5, ws)))
+	p3 := and(ws, p4, ws, zeroOrMore(and(o3, p4, ws)))
+	p2 := and(ws, p3, ws, zeroOrMore(and(o2, p3, ws)))
+	p1 := and(ws, p2, ws, zeroOrMore(and(o1, p2, ws)))
+	cond1 := and(ws, _if, ws, charLiteral('('), ws, p1, ws, charLiteral(')'), ws, p1, ws, stringLiteral("else"), ws, p1, ws)
+	cond2 := and(p1, optional(and(charLiteral('?'), ws, p1, ws, charLiteral(':'), ws, p1, ws)))
+	cond.value = or(cond1, cond2)
+
+	num.SetCreateNode(ast.CreateDoubleOperand)
+	boolean.SetCreateNode(ast.CreateBooleanOperand)
+	_string.SetCreateNode(ast.CreateStringOperand)
+
+	funcname.SetCreateNode(ast.CreateIdentifier)
+	_if.SetCreateNode(ast.CreateIdentifier)
+
+	neg.SetCreateNode(ast.CreateUnaryOperation)
+
 	add.SetCreateNode(ast.CreateBinaryOperation)
 	sub.SetCreateNode(ast.CreateBinaryOperation)
 	mul.SetCreateNode(ast.CreateBinaryOperation)
 	div.SetCreateNode(ast.CreateBinaryOperation)
+	mod.SetCreateNode(ast.CreateBinaryOperation)
 	exp.SetCreateNode(ast.CreateBinaryOperation)
-	neg.SetCreateNode(ast.CreateUnaryOperation)
-	funcName.SetCreateNode(ast.CreateIdentifier)
-	term.SetCreateNode(ast.CreateBinaryLeftAssoc)
-	expression.SetCreateNode(ast.CreateBinaryLeftAssoc)
-	power.SetCreateNode(ast.CreateBinaryRightAssoc)
+
+	a.SetCreateNode(ast.CreateBinaryOperation)
+	o.SetCreateNode(ast.CreateBinaryOperation)
+	xor.SetCreateNode(ast.CreateBinaryOperation)
+	not.SetCreateNode(ast.CreateUnaryOperation)
+
+	lt.SetCreateNode(ast.CreateBinaryOperation)
+	le.SetCreateNode(ast.CreateBinaryOperation)
+	gt.SetCreateNode(ast.CreateBinaryOperation)
+	ge.SetCreateNode(ast.CreateBinaryOperation)
+
+	p7.SetCreateNode(ast.CreateUnaryPrefix)
+	p6.SetCreateNode(ast.CreateBinaryRightAssoc)
+	p5.SetCreateNode(ast.CreateBinaryLeftAssoc)
+	p4.SetCreateNode(ast.CreateBinaryLeftAssoc)
+	p3.SetCreateNode(ast.CreateBinaryLeftAssoc)
+	p2.SetCreateNode(ast.CreateBinaryLeftAssoc)
+	p1.SetCreateNode(ast.CreateBinaryLeftAssoc)
+
 	function.SetCreateNode(ast.CreateFunction)
-	operand.SetCreateNode(ast.CreateUnaryPrefix)
+	cond1.SetCreateNode(ast.CreateFunction)
+	cond2.SetCreateNode(ast.CreateTernaryConditional)
 
-	group.SetId("group")
-	operand.SetId("factor")
-	term.SetId("term")
-	power.SetId("power")
-	number.SetId("number")
-	add.SetId("add")
-	sub.SetId("sub")
-	mul.SetId("mul")
-	div.SetId("div")
-	exp.SetId("exp")
-	neg.SetId("neg")
-	expression.SetId("expression")
-
-	return expression
+	return cond
 }
