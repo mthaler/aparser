d265234b12e296ec67649b8bdf13be1117bc204a
Michael Thaler <michael.thaler@dlr.de>
2021-01-02 12:39:01 +0100

    Added more tests.

--- eval_test.go
+++ eval_test.go
@@ -12,4 +12,25 @@ func TestEval(t *testing.T) {
 	if r != 7.0 {
 		t.Error("Result should be 7.0")
 	}
+	r, err = Eval("3 + 4 + 5")
+	if err != nil {
+		t.Error(err)
+	}
+	if r != 12.0 {
+		t.Error("Result should be 12.0")
+	}
+	r, err = Eval("2 * 3 + 4")
+	if err != nil {
+		t.Error(err)
+	}
+	if r != 10.0 {
+		t.Error("Result should be 10.0")
+	}
+	r, err = Eval("2 + 3 * 4")
+	if err != nil {
+		t.Error(err)
+	}
+	if r != 14.0 {
+		t.Error("Result should be 14.0")
+	}
 }
