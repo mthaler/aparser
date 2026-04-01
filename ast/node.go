e7b939bbe7cea330d74e7496777e911430f8fd50
Michael Thaler <michael.thaler2@gmail.com>
2026-04-01 10:31:44 +0200

    Fixed spelling error.

--- ast/node.go
+++ ast/node.go
@@ -1,5 +1,5 @@
 /*
-Package ast implements a simple library for building ab abstract syntax tree for arithmetic expressions.
+Package ast implements a simple library for building an abstract syntax tree for arithmetic expressions.
 
 There are two types of nodes: operands and operations. An operand represents values, e.g. a double, a boolean or a string.
 Operations represent arithmetic operations, e.g. addition, subtraction etc.
