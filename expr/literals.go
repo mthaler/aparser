86821448b3fff956834e78f208b1b52a04e3a02b
Michael Thaler <michael.thaler2@gmail.com>
2026-04-01 11:09:10 +0200

    Added stringliteral.go.

--- expr/literals.go
+++ expr/literals.go
@@ -33,29 +33,6 @@ func (d doubleLiteralExpression) parse(buffer *buffer) bool {
 	return false
 }
 
-type stringLiteralExpression struct {
-	*abstractExpression
-	str string
-}
-
-func stringLiteral(s string) stringLiteralExpression {
-	a := abstractExpression{}
-	return stringLiteralExpression{abstractExpression: &a, str: s}
-}
-
-func (s stringLiteralExpression) parse(buffer *buffer) bool {
-	if !buffer.hasMoreChars() {
-		return false
-	} else if len(buffer.rest()) < len(s.str) {
-		return false
-	} else if string(buffer.rest()[:len(s.str)]) == s.str {
-		buffer.incrementCurrentPositionBy(len(s.str))
-		return true
-	} else {
-		return false
-	}
-}
-
 type caseInsensitiveStringLiteralExpression struct {
 	*abstractExpression
 	str string
